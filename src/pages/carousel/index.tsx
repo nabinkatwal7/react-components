57427b16a5c337d6880bd5160d205c6a55cf7273
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2023-10-05 12:47:19 +0545

    feature: carousel

---
+++ src/pages/carousel/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Carousel from "@/Components/Carousel";
+import styles from "../../Components/carousel.module.css";
+
+function Index() {
+    const images = [
+        "https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/632522/pexels-photo-632522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/777059/pexels-photo-777059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    ];
+    return (
+        <div className="App">
+            <header className={styles.AppHeader}>
+                <h1>Image Carousel using React and Framer Motion</h1>
+            </header>
+            <main>
+                <Carousel images={images} />
+            </main>
+        </div>
+    );
+}
+
+export default Index;
